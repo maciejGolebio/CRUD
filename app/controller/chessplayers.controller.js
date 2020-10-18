61314fafc5319bb29488b301d1742eaa37ee04c4
maciek666 <czerwoneszelki19@gmail.com>
2020-10-18 11:56:30 +0200

    delete copy paste comments

--- app/controller/chessplayers.controller.js
+++ app/controller/chessplayers.controller.js
@@ -2,10 +2,7 @@ const db = require("../model");
 const chessPlayers = db.chessplayers;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new Tutorial
 exports.create = (req, res) => {
-  // Validate request
-
   if (!req.body.name) {
     res.status(400).send({
       message: "Content can not be empty!"
@@ -30,7 +27,6 @@ exports.create = (req, res) => {
     });
 };
 
-// Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
     const name = req.query.name;
     let condition = name ? { name: { [Op.iLike]: `%${name}%` } } : null;
@@ -47,22 +43,19 @@ exports.findAll = (req, res) => {
       });
 };
 
-// Find a single Tutorial with an id
 exports.findOne = (req, res) => {
   
 };
 
-// Update a Tutorial by the id in the request
 exports.update = (req, res) => {
   
 };
 
-// Delete a Tutorial with the specified id in the request
+
 exports.delete = (req, res) => {
   
 };
 
-// Delete all Tutorials from the database.
 exports.deleteAll = (req, res) => {
   
 };
